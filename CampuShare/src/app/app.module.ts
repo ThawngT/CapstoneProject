819b8dbf16bcd51e69c39afe67d9421c1565cb5f
thawng <ttha9806@conestogac.on.ca>
2024-02-08 15:39:20 -0500

    initial components added

--- CampuShare/src/app/app.module.ts
+++ CampuShare/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/pages/header/header.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { FooterComponent } from './components/pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
